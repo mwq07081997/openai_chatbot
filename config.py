ae59604ea2bf7054d14caf969e1f41908df75999
mwq07081997 <38372817+mwq07081997@users.noreply.github.com>
2023-01-28 12:53:40 +0800

    API key is here
    removed api key for security reasons

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
-OPENAI_API_KEY = "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"
+OPENAI_API_KEY = "TEMP_API_KEY_TO_BE_PROVIDED"
 
 # temporary openai api key. do not share
 # store other api keys here, web3.storage 
