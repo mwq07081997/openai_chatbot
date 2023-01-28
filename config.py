c4abc80c12cc8b52964dcaa384cc8efcdf3f910d
mwq07081997 <38372817+mwq07081997@users.noreply.github.com>
2023-01-28 12:47:22 +0800

    contains api key (temporary use)
    API key for OpenAI and web3.storage, etc.

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
 OPENAI_API_KEY = "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"
 
 # temporary openai api key. do not share
-# store other api keys here, web3.storage
+# store other api keys here, web3.storage 
